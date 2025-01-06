480c1aeea11f1617810ab2ca079706a06a013a5b
Al Hakimi <alhakimihaikal4106@gmail.com>
2025-01-06 18:10:39 +0800

    aa

--- assets/js/ggg.js
+++ assets/js/ggg.js
@@ -1,6 +1,12 @@
-// This script enables the "View More" interaction
 document.querySelectorAll('.view-more').forEach(button => {
     button.addEventListener('click', function() {
-        alert('Additional information will be displayed here.');
+        const moreContent = this.nextElementSibling;
+        if (moreContent.style.display === 'none') {
+            moreContent.style.display = 'block';
+            this.innerText = 'View Less'; // Change button text to View Less
+        } else {
+            moreContent.style.display = 'none';
+            this.innerText = 'View More'; // Change button text back to View More
+        }
     });
 });
